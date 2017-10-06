5ef337242fbbee467d9788bafa7cc5594a7ad969
Pablo <pabloandresvdl@gmail.com>
2017-10-07 00:56:22 +0100

    first commit, add Dishes class

---
+++ lib/dishes.rb
@@ -0,0 +1,18 @@
+class Dishes #:nodoc:"
+  def initialize(menu)
+    @menu = list_struct(menu)
+  end
+
+  def show_menu_with_price
+    @menu.each do |x|
+    puts "#{x.dish}: £#{x.price}"
+  end
+ end
+
+DishesMenu = Struct.new(:dish, :price)
+  def list_struct(dishes_price)
+    dishes_price.map do |item|
+     DishesMenu.new(item[0], item[1])
+  end
+end
+end
---
+++ spec/dishes_spec.rb
@@ -0,0 +1,11 @@
+require 'dishes'
+describe Dishes do
+
+  menu = [["chicken", 10]]
+  subject { Dishes.new(menu) }
+
+  it 'expects to output a menu with a list of items and price' do
+    expect { subject.show_menu_with_price }.to output("chicken: £10\n").to_stdout
+  end
+
+end
