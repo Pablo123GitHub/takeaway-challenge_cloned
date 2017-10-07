3f00bcb3ef1d90bfe84de377acead396011abd1a
Pablo <pabloandresvdl@gmail.com>
2017-10-07 09:10:04 +0100

    reformat w rubocop and add --format documentation in .rspec

--- lib/dishes.rb
+++ lib/dishes.rb
@@ -5,14 +5,14 @@ class Dishes #:nodoc:"
 
   def show_menu_with_price
     @menu.each do |x|
-    puts "#{x.dish}: £#{x.price}"
+      puts "#{x.dish}: £#{x.price}"
+    end
   end
- end
 
 DishesMenu = Struct.new(:dish, :price)
-  def list_struct(dishes_price)
-    dishes_price.map do |item|
+ def list_struct(dishes_price)
+   dishes_price.map do |item|
      DishesMenu.new(item[0], item[1])
-  end
-end
+   end
+ end
 end
--- spec/dishes_spec.rb
+++ spec/dishes_spec.rb
@@ -1,11 +1,10 @@
 require 'dishes'
 describe Dishes do
-
-  menu = [["chicken", 10]]
+  menu = [['chicken', 10]]
   subject { Dishes.new(menu) }
 
   it 'expects to output a menu with a list of items and price' do
-    expect { subject.show_menu_with_price }.to output("chicken: £10\n").to_stdout
+    expect { subject.show_menu_with_price }.to \
+     output("chicken: £10\n").to_stdout
   end
-
 end
