530fb5947ac655ad6584248bb02de9be18ba67a5
Pablo <pabloandresvdl@gmail.com>
2017-10-08 21:38:25 +0100

    add print_nice in the Menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,13 @@
 
 class Menu #:nodoc:"
+  attr_reader :menu
   def initialize(menu)
     @menu = menu
+  end
+
+  def print_nice
+    menu.map do |dish, price|
+      format('%s £%.2f', dish.to_s.capitalize, price)
+    end.join(', ')
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,15 @@ require 'menu'
 require 'takeaway'
 
 describe Menu do
+  let(:menu) { described_class.new(menu_hash) }
+  let(:menu_hash) do
+    {
+      chicken: 10,
+      fish: 8
+    }
+  end
+
+  it 'prints nicely the dishes on the menu' do
+    expect(menu.print_nice).to eq 'Chicken £10.00, Fish £8.00'
+  end
 end
